03d8544435ffffe3262e89e266e93656fab4d692
root <root@ip-172-31-16-101.ap-southeast-1.compute.internal>
2018-11-12 09:38:48 +0000

    adding mystring.py

---
+++ pythonscripts/mystring.py
@@ -0,0 +1,13 @@
+#a = "Sreenivasa"
+#l = len(a)
+#print (1)
+#print(dir(str))
+
+#print (help(a.lower))
+
+#print(a.lower())
+#print(a.islower())
+#print(a.isuper())
+#print(a.upper())
+#print(a.isalpha())
+
